6df2c2d1d964703a1e9f8ebce27d6ec0d2e78c46
Sriram Ramesh <sriram@vananam.com>
2026-03-13 11:43:01 +0530

    CH2: Gameloop L3: Game loop

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
 def main():
     VERSION = pygame.version.ver
     print(f"Starting Asteroids with pygame version:{VERSION}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
