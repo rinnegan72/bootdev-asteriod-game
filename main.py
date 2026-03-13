2ed2c9dde7c7c65dbebee691b01318a5d6f8e3f7
Sriram Ramesh <sriram.ramesh312@gmail.com>
2026-03-13 07:28:18 +0530

    CH2: Gameloop L1: Modules
    added resollution variables to constant.py
    added import and used these in main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 def main():
     VERSION = pygame.version.ver
     print(f"Starting Asteroids with pygame version:{VERSION}")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
