9d98a58b08cfdb731b54d694808bcd6bfa54d169
Sriram Ramesh <sriram@vananam.com>
2026-03-14 16:34:54 +0530

    CH2: Gameloop L4: FPS

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Starting Asteroids with pygame version:{VERSION}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -15,6 +17,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        # print(dt)
 
 
 if __name__ == "__main__":
