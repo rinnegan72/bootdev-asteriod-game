3cf74dbca2e6acede891c975b6abecc99a47c4d2
Sriram Ramesh <sriram@vananam.com>
2026-03-15 12:45:42 +0530

    CH3: Player L3: Moving Around

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
+        player.update(dt)
         player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
