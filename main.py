aeb83a2319a7e8375411017c5923e21abe439c68
Sriram Ramesh <sriram@vananam.com>
2026-03-15 15:57:08 +0530

    CH3: Player L5: Groups

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for drawables in drawable:
+            drawables.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
